3f0a0af2eefdceb86f2396bc42988807fd04ac68
Eelapanti Mythri <eelapantimythri@gmail.com>
2026-03-14 11:54:39 +0530

    Update app.py

--- helmet/app.py
+++ helmet/app.py
@@ -105,7 +105,8 @@ def result_image():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 # from flask import Flask, render_template, request
 # from ultralytics import YOLO
